aae76028b0ee983e21998b369d4fd6e2c53ff418
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-06 12:30:54 -0700

    got a first pass solution for sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -13,3 +13,23 @@ def f(x):
 
 # Your code here
 
+# create a list of possible combinations
+# check each possible list of combinations in the formula
+combos = {}
+count = 0
+for x in q:
+    for y in q:
+        count+=1
+        combos[count] = (x, y)
+print(combos)
+
+for add in combos:
+    for sub in combos:
+        if f(combos[add][0]) + f(combos[add][1]) == f(combos[sub][0]) - f(combos[sub][1]):
+            x_add = f(combos[add][0])
+            y_add = f(combos[add][1])
+            x_sub = f(combos[sub][0])
+            y_sub = f(combos[sub][1])
+            print(f"f({combos[add][0]}) + f({combos[add][1]}) = f({combos[sub][0]}) - f({combos[sub][1]})  {x_add} + {y_add} = {x_sub} - {y_sub}")
+        
+        
